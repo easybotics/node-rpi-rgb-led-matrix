fa3db6dbf55a4bf2a50739b3f7569695b2d78d2b
Cameron Owen <cameron.owen@live.com.au>
2020-04-29 08:16:31 +1000

    Removed unecessary init call

--- src/ledmatrix.cc
+++ src/ledmatrix.cc
@@ -55,7 +55,7 @@ LedMatrix::LedMatrix (int rows, int cols, int parallel_displays, int chained_dis
 	defaults.hardware_mapping = mapping;
 	defaults.led_rgb_sequence = rgbseq;
 
-	std::cout << rgb_matrix::ParseOptionsFromFlags(&num, &d, &defaults, &runtime, true) << std::endl;
+	//std::cout << rgb_matrix::ParseOptionsFromFlags(&num, &d, &defaults, &runtime, true) << std::endl;
 	//temp debug output
 	//parse extra settings flags for POWER_USERS (TM)
 
